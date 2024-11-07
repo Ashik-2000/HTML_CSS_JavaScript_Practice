6cb67ed11ab75c7e85c9992d62312536a62c84e4
Ashikul Islam <ashikulislamfb@gmail.com>
2024-11-08 00:21:15 +0600

    practice

--- Practice/script.js
+++ Practice/script.js
@@ -1,6 +1,8 @@
 let mobiles = document.getElementById("mobile");
 let laptops = document.getElementById("laptop");
-let items = laptops.getElementsByClassName("item");
+let items = document.querySelectorAll(".item:nth-child(2)");
+
+// items.style.color = 'red'
 
 for (let i = 0; i < items.length; i++){
     items[i].style.color = 'red'
