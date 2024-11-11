73e8e1db70d661618e6f1d7d17146f2d1daf1f0c
Ashikul Islam <ashikulislamfb@gmail.com>
2024-11-11 10:05:48 +0600

    p

--- Practice/script.js
+++ Practice/script.js
@@ -1,22 +1,3 @@
-let newTask = document.querySelector('#new-task')
-let form = document.querySelector('form')
-let todoUL = document.querySelector('#items')
-let completeUL = document.querySelector('.complete-list ul')
-
-let createTast = function (task) {
-    let listItem = document.createElement('li')
-    let checkBox = document.createElement('input')
-    let label = document.createElement('label')
-
-    label.innerText = task
-    checkBox.type = 'checkbox'
-
-    listItem.appendChild(checkBox)
-    listItem.appendChild(label)
-
-    return listItem
-}
-
-let addTast = function (event) {
-    let listItem = createTast(newTask.value)
-}
+let number = () => 10;
+    
+console.log(number())
