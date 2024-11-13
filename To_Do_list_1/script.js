dbecf95a13779ff8c50b2db37ac369a96c6c84b1
Ashikul Islam <ashikulislamfb@gmail.com>
2024-11-13 13:37:57 +0600

    practice

--- To_Do_list_1/script.js
+++ To_Do_list_1/script.js
@@ -1,16 +1,22 @@
 let input = document.querySelector('.inputField');
 let btn = document.querySelector('.addBtn');
 let taskContainer = document.querySelector('.taskContainer');
+let completedTask = document.querySelector('.completedTask');
 
-btn.addEventListener('click',function(){
+const addTask = function(){
     var task = document.createElement('p');
     task.innerText = input.value;
     taskContainer.appendChild(task);
     input.value = '';
-    task.addEventListener('click',function(){
+    task.addEventListener('click',() => {
         task.style.textDecoration = 'line-through';
-    })
-    task.addEventListener('dblclick',function(){
+    });
+    task.addEventListener('dblclick',function () {
         taskContainer.removeChild(task);
-    })
-})
+        completedTask.appendChild(task);
+    });
+}
+
+
+
+btn.addEventListener('click', addTask);
