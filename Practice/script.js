c25b7bd25ba8b781b56d1a98a946e78cb7b8b8e1
Ashikul Islam <ashikulislamfb@gmail.com>
2024-11-14 18:43:01 +0600

    p

--- Practice/script.js
+++ Practice/script.js
@@ -1,12 +1,3 @@
-let input = document.querySelector('.input');
-let result = document.querySelector('.result');
-let thanks = document.querySelector('.thanks');
-
-function show () {
-    result.innerHTML = this.value;
-    setTimeout(() => {
-        thanks.innerHTML = `You have typed: ${this.value}`;
-    },1000);
-}
-
-input.addEventListener('keyup',show);
+let age = 8;
+let type = age >= 18? 'Adult' : age < 10? 'Child': 'Young';
+console.log(type);
