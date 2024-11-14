03e5ad822d36a0c61f1c2a17cd731c52bec232c4
Ashikul Islam <ashikulislamfb@gmail.com>
2024-11-14 21:36:53 +0600

    todolws

--- Practice/script.js
+++ Practice/script.js
@@ -1,3 +1,14 @@
-let age = 8;
-let type = age >= 18? 'Adult' : age < 10? 'Child': 'Young';
-console.log(type);
+
+var numbers = [1, 5, 6, 7, 2, 6, 9, 8, 3];
+// var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+var show = numbers.reduce(function(previousValue, currentValue){
+    return previousValue + currentValue;
+});
+
+console.log(show);
+// numbers.reduce(function(previousValue, currentValue){
+//     console.log(previousValue);
+//     console.log(currentValue);
+// });
