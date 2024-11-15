e304ce58d3e186a194c410a003ac94f95f12fabe
Ashikul Islam <ashikulislamfb@gmail.com>
2024-11-16 00:04:07 +0600

    todolist

--- To_Do_list_1/script.js
+++ To_Do_list_1/script.js
@@ -1,9 +1,9 @@
 let input = document.querySelector('.inputField');
 let form = document.querySelector('form');
-let tasks = document.querySelector('#dueTasks');
-let completedTask = document.querySelector('#doneTasks');
+let dueTasks = document.querySelector('#dueTasks');
+let doneTasks = document.querySelector('#doneTasks');
 
-let createTask = function(task){
+let createTask = function (task) {
     let listItem = document.createElement('li');
     let checkbox = document.createElement('input');
     let label = document.createElement('label');
@@ -17,12 +17,42 @@ let createTask = function(task){
     return listItem;
 }
 
-let addTask = function(event){
+let addTask = function (event) {
     event.preventDefault();
     let listItem = createTask(input.value);
-    tasks.appendChild(listItem);
+    dueTasks.appendChild(listItem);
     input.value = "";
+    clickCheckBox(listItem, completedTask);
 }
+
+let completedTask = function () {
+    let listItem = this.parentNode;
+    let deleteBtn = document.createElement('button');
+    deleteBtn.innerText = 'Delete';
+    deleteBtn.className = 'deleteBtn';
+
+    listItem.appendChild(deleteBtn);
+
+    let checkbox = listItem.querySelector('input[type="checkbox"]');
+    checkbox.remove();
+    doneTasks.appendChild(listItem);
+    clickDeleteBtn(listItem, deleteTask);
+}
+
+let deleteTask = function() {
+    let listItem = this.parentNode;
+    doneTasks.removeChild(listItem);
+}
+
+let clickCheckBox = function(listitem, completetask){
+    let checkBox = listitem.querySelector('input[type="checkbox"]');
+    checkBox.onchange = completetask;
+}
+let clickDeleteBtn = function(listitem, deletetask){
+    let deleteBtn = listitem.querySelector('button');
+    deleteBtn.onclick = deletetask;
+}
+
 
 
 form.addEventListener('submit', addTask);
