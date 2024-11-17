2921c9b9a02f24bb828b41e4124ca4c316a45ef3
Ashikul Islam <ashikulislamfb@gmail.com>
2024-11-17 21:18:13 +0600

    to

--- Practice/script.js
+++ Practice/script.js
@@ -1,14 +1,6 @@
+let a = 10;
+let b = 20;
 
-var numbers = [1, 5, 6, 7, 2, 6, 9, 8, 3];
-// var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+[b, a] = [a, b];
 
-
-var show = numbers.reduce(function(previousValue, currentValue){
-    return previousValue + currentValue;
-});
-
-console.log(show);
-// numbers.reduce(function(previousValue, currentValue){
-//     console.log(previousValue);
-//     console.log(currentValue);
-// });
+// console.log(a, b);
