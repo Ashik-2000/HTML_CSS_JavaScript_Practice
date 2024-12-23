1e4fdbda34fdfdab5544419eead3284926a3bced
Ashikul Islam <ashikulislamfb@gmail.com>
2024-12-23 12:25:40 +0600

    regular expression

--- Practice/script.js
+++ Practice/script.js
@@ -1,5 +1,5 @@
 let a = '123456789'
 let b = '987654321'
-let dog = 'I love dogs.' 
-let cat = dog.replace('dogs', 'cats')
+let dog = 'I love Dogs.' 
+let cat = dog.replace(/dogs/i, 'cats')
 console.log(cat)
