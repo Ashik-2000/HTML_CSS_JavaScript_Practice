de50225af6730061445f8edd788bbb8e62894408
Ashikul Islam <ashikulislamfb@gmail.com>
2024-12-23 12:24:37 +0600

    replace

--- Practice/script.js
+++ Practice/script.js
@@ -1,5 +1,5 @@
 let a = '123456789'
 let b = '987654321'
-console.log(a.slice(-7,-3))
-console.log(b.slice(3,7))
-console.log(b.substr(3,4))
+let dog = 'I love dogs.' 
+let cat = dog.replace('dogs', 'cats')
+console.log(cat)
