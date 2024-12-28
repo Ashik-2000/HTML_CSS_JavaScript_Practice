ef4041420da95e0b82d6b4fce48e8a6bb28c8f99
Ashikul Islam <ashikulislamfb@gmail.com>
2024-12-28 11:33:26 +0600

    map

--- Practice/script.js
+++ Practice/script.js
@@ -1,10 +1,10 @@
 let array = [5, 6, 9, 7, 3, 2, 1, 8, 4]
 
 function myFunction(value, index, array){
-    console.log(value);
-    console.log(index);
-    console.log('----------');
+    return value * 2; 
 }
 
-array.forEach(myFunction)
+let numbers = array.map(myFunction)
+console.log(array);
+console.log(numbers);
 
