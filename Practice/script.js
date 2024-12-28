c39c47b6d72c76f6550a0ad1dcbfe9377fed5dd2
Ashikul Islam <ashikulislamfb@gmail.com>
2024-12-28 11:34:59 +0600

    filter

--- Practice/script.js
+++ Practice/script.js
@@ -1,10 +1,10 @@
 let array = [5, 6, 9, 7, 3, 2, 1, 8, 4]
 
 function myFunction(value, index, array){
-    return value * 2; 
+    return value > 4; 
 }
 
-let numbers = array.map(myFunction)
+let numbers = array.filter(myFunction)
 console.log(array);
 console.log(numbers);
 
