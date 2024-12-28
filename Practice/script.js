253370a9547e38fe7eeb6ee22462f4c5956664dc
Ashikul Islam <ashikulislamfb@gmail.com>
2024-12-28 11:31:03 +0600

    foreach

--- Practice/script.js
+++ Practice/script.js
@@ -1,6 +1,10 @@
 let array = [5, 6, 9, 7, 3, 2, 1, 8, 4]
-let leargest = Math.max.apply(null, array) 
-let lowest = Math.min.apply(null, array) 
-console.log(leargest);
-console.log(lowest);
+
+function myFunction(value, index, array){
+    console.log(value);
+    console.log(index);
+    console.log('----------');
+}
+
+array.forEach(myFunction)
 
