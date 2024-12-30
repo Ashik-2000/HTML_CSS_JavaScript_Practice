f5dc69021404ba71751e74c72effab42e12cf53d
Ashikul Islam <ashikulislamfb@gmail.com>
2024-12-30 17:22:57 +0600

    ha ha ha

--- Practice/script.js
+++ Practice/script.js
@@ -1,11 +1,3 @@
-let array = [5, 6, 9, 7, 3, 2, 1, 8, 4]
-
-function myFunction(total, value, index, array){
-    console.log(total);
-    return total + value;
-    
-}
-
-let number = array.reduce(myFunction)
-console.log(number);
-
+let d = new Date();
+d = d.toISOString()
+console.log(d);
