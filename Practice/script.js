807c88eabe64f992635bf8905109d0002cce7b32
Ashikul Islam <ashikulislamfb@gmail.com>
2024-12-31 20:59:29 +0600

    for loops

--- Practice/script.js
+++ Practice/script.js
@@ -1,7 +1,14 @@
-var i = 5;
+var arr = [1, 2, 3, 5, 4, 6, 7, 8, 9]
 
-for (var i = 0; i < 10; i++) {
-    console.log(i);
+for (x in arr){
+    console.log(arr[x]);
+    
 }
 
-console.log(i);
+console.log('\n------------------\n');
+
+
+for (x of arr){
+    console.log(x);
+    
+}
