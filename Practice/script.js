ec5d6e4b1a052bef8abfc4cdcb59606a1578c4c4
Ashikul Islam <ashikulislamfb@gmail.com>
2024-12-31 15:17:22 +0600

    switch

--- Practice/script.js
+++ Practice/script.js
@@ -1,58 +1,12 @@
-function leapYear1(a) {
-    const year = Number(a)
-    if (isNaN(year)) {
-        console.log(a + ' is not a number');
-    }
-    else if (!Number.isInteger(year)) {
-        console.log(year + ' is not a Integer');
-    }
-    else {
-        if ((year % 4 == 0) || (year % 100 == 0 && year % 400 == 0)) {
-            console.log(year + ' is a leapYear');
-        }
-        else {
-            console.log(year + ' is not a leapYear');
-        }
-    }
-}
+let n = 5;
 
-function leapYear2(a) {
-    const year = Number(a); // Convert input to a number
-    if (isNaN(year)) {
-        console.log(a + ' is not a valid number');
-    } else if (!Number.isInteger(year)) {
-        console.log(a + ' is not an integer'); 
-    } else {
-        // Correct leap year calculation
-        if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
-            console.log(year + ' is a leap year');
-        } else {
-            console.log(year + ' is not a leap year');
-        }
-    }
-}
-
-console.log('My function');
-
-leapYear1(1900);
-
-
-console.log('ChatGPT');
-
-leapYear2(1900);
-
-let n = 1900;
-
-if (n % 4 ==0){
-    if (n % 100 == 0) {
-        console.log('Not LeapYear');
-    }
-    else if (n % 400 ==0){
-        console.log('LeapYear');
-        
-    }
-}
-else{
-    console.log('Not LeapYear');
-    
+switch (true) {
+    case n > 10:
+        console.log('This is greter than 15');
+        break;
+    case n < 10:
+        console.log('This is smaller then 10');
+        break;
+    default:
+        console.log('This is 10');
 }
