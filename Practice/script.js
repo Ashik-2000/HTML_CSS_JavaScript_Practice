7fccadeb05c0d350ff849e236ac33967b75ea377
Ashikul Islam <ashikulislamfb@gmail.com>
2024-12-31 19:41:07 +0600

    var & let

--- Practice/script.js
+++ Practice/script.js
@@ -1,12 +1,7 @@
-let n = 5;
+var i = 5;
 
-switch (true) {
-    case n > 10:
-        console.log('This is greter than 15');
-        break;
-    case n < 10:
-        console.log('This is smaller then 10');
-        break;
-    default:
-        console.log('This is 10');
+for (var i = 0; i < 10; i++) {
+    console.log(i);
 }
+
+console.log(i);
