2a1b14e41c77ee41979aa76940dd6c186dddd179
Ashikul Islam <ashikulislamfb@gmail.com>
2025-01-10 21:30:45 +0600

    function

--- Practice/script.js
+++ Practice/script.js
@@ -1,4 +1,22 @@
-(function printThis(){
-    console.log('This is a slef invoked function.');
+function printThis(x, o){
+    x = x * 5;
+    o.p = 10;
+
+    console.log(x);
+    console.log(o.p);
     
-})(); 
+
+    
+}
+
+let obj = {
+    p : 1,
+    q : 2
+}
+
+let x = 5;
+
+printThis(x, obj);
+
+console.log(x);
+console.log(obj);
