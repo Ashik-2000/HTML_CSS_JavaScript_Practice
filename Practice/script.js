f86e739dd3821a2ac285d58948177fba28a68e5d
Ashikul Islam <ashikulislamfb@gmail.com>
2025-01-14 19:28:53 +0600

    class

--- Practice/script.js
+++ Practice/script.js
@@ -1,21 +1,18 @@
-function Person(name, age){
-    this.name = name;
-    this.age = age;
-}
-
-Person.prototype = {
+class Person {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
     eat() {
         console.log('Person is eating');
-    },
+    }
     sleep() {
         console.log('Person is sleeping');
-    },
+    }
     play() {
         console.log('Person is playing');
-    },
+    }
 }
-
-
 
 const sakib = new Person("Sakib", 35);
 sakib.play();
